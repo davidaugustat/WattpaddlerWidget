3d6c17d000fcbbca302e0d1e1b87742d43e1eccc
David Augustat <code@davidaugustat.com>
2022-07-07 00:15:08 +0200

    Created method to update widget and insert dummy data

--- app/src/main/java/de/davidaugustat/wattpaddlerwidget/MainWidget.java
+++ app/src/main/java/de/davidaugustat/wattpaddlerwidget/MainWidget.java
@@ -5,6 +5,9 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 /**
  * Implementation of App Widget functionality.
  */
@@ -12,10 +15,10 @@ public class MainWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main_widget);
+
+        updateWidgetContents(views, context);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -37,5 +40,36 @@ public class MainWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    private static void updateWidgetContents(RemoteViews views, Context context){
+        TidesInfo tidesInfo = getDummyTidesInfo();
+        views.setTextViewText(R.id.textViewLocation, tidesInfo.getLocationName());
+        views.setTextViewText(R.id.textViewDate, tidesInfo.getDateFormatted());
+
+        String highTideText = String.format(context.getString(R.string.high_tide_text),
+                tidesInfo.getHighTide1(), tidesInfo.getHighTide2());
+        views.setTextViewText(R.id.textViewHighTide, highTideText);
+
+        String lowTideText = String.format(context.getString(R.string.low_tide_text),
+                tidesInfo.getLowTide1(), tidesInfo.getLowTide2());
+        views.setTextViewText(R.id.textViewLowTide, lowTideText);
+
+        String lastUpdatedText = String.format(context.getString(R.string.last_updated_text),
+                tidesInfo.getLastUpdatedTimeFormatted());
+        views.setTextViewText(R.id.textViewLastUpdated, lastUpdatedText);
+    }
+
+    private static TidesInfo getDummyTidesInfo(){
+        return new TidesInfo(
+                "123214",
+                "Bremerhaven / Alter Leuchtturm",
+                LocalDate.now(),
+                "10:00",
+                "22:30",
+                "16:00",
+                "4:30",
+                LocalDateTime.now()
+        );
     }
 }
