bdb3a894718eba85e21784c1d4b8a2b49726cd86
David Augustat <st166284@stud.uni-stuttgart.de>
2022-07-14 13:02:17 +0200

    Started to implement DateTimeHelper.java

---
+++ app/src/main/java/de/davidaugustat/wattpaddlerwidget/DateTimeHelper.java
@@ -0,0 +1,8 @@
+package de.davidaugustat.wattpaddlerwidget;
+
+public class DateTimeHelper {
+    public static String getCurrentDateInQueryNotation(){
+        // TODO: Implement this
+        return "2022-07-12";
+    }
+}
