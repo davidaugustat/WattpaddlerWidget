10140104d9d917b0629f72cc561f2602f9dfb97c
David Augustat <st166284@stud.uni-stuttgart.de>
2022-07-14 13:09:09 +0200

    Code for updating the widget from API data.

--- app/src/main/java/de/davidaugustat/wattpaddlerwidget/MainWidget.java
+++ app/src/main/java/de/davidaugustat/wattpaddlerwidget/MainWidget.java
@@ -3,6 +3,7 @@ package de.davidaugustat.wattpaddlerwidget;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import java.time.LocalDate;
@@ -18,10 +19,8 @@ public class MainWidget extends AppWidgetProvider {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main_widget);
 
-        updateWidgetContents(views, context);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        refreshWidget(views, context, appWidgetManager, appWidgetId, isManualOrInitialRefresh());
+        Log.d("Updating widget", "Updating widget");
     }
 
     @Override
@@ -42,25 +41,103 @@ public class MainWidget extends AppWidgetProvider {
         // Enter relevant functionality for when the last widget is disabled
     }
 
-    private static void updateWidgetContents(RemoteViews views, Context context){
-        TidesInfo tidesInfo = getDummyTidesInfo();
+    /**
+     * Loads the data from the API and displays it on the widget.
+     *
+     * @param views RemoteViews representing the widget
+     * @param context Context used to get strings
+     * @param appWidgetManager AppWidgetManager used to update the widget
+     * @param appWidgetId ID of the widget to update
+     * @param isManualOrInitialUpdate true iff the widget was manually or initially updated. In this
+     *                                case a info message "Network error" is displayed on the widget
+     *                                to inform the user about the error. If the widget was
+     *                                automatically updated, no error message is displayed but the
+     *                                widget remains unchanged (continues to display old data).
+     */
+    private static void refreshWidget(RemoteViews views, Context context, AppWidgetManager appWidgetManager, int appWidgetId,
+                                      boolean isManualOrInitialUpdate) {
+        new DataFetcher(context).fetchTidesDataSingleDay(getLocation(), tidesInfo -> {
+                    updateWidgetLayout(views, context, appWidgetManager, appWidgetId, tidesInfo);
+                    Log.d("Tides Info", tidesInfo.toString());
+                },
+                errorMessage -> {
+                    if (isManualOrInitialUpdate) {
+                        updateWidgetLayoutAtError(views, context, appWidgetManager, appWidgetId);
+                    }
+                    Log.e("Error fetching tides", errorMessage);
+                });
+    }
+
+    /**
+     * Displays a "Network error" message on the widget. This can be used to inform the user about
+     * an error that ocurred during update.
+     *
+     * @param views RemoteViews representing the widget
+     * @param context Context used to get strings
+     * @param appWidgetManager AppWidgerManager used to update the widget.
+     * @param appWidgetId ID of the widget.
+     */
+    private static void updateWidgetLayoutAtError(RemoteViews views, Context context,
+                                                  AppWidgetManager appWidgetManager, int appWidgetId) {
+        views.setTextViewText(R.id.textViewLocation, context.getString(R.string.no_network_text));
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    /**
+     * Displays the new tides data from the tidesInfo parameter on the widget.
+     *
+     * @param tidesInfo Tides info that should be displayed on the widget.
+     */
+    private static void updateWidgetLayout(RemoteViews views, Context context,
+                                           AppWidgetManager appWidgetManager, int appWidgetId,
+                                           TidesInfo tidesInfo) {
         views.setTextViewText(R.id.textViewLocation, tidesInfo.getLocationName());
         views.setTextViewText(R.id.textViewDate, tidesInfo.getDateFormatted());
 
-        String highTideText = String.format(context.getString(R.string.high_tide_text),
-                tidesInfo.getHighTide1(), tidesInfo.getHighTide2());
+        String highTideText;
+        if (tidesInfo.getHighTide2() == null) {
+            highTideText = String.format(context.getString(R.string.high_tide_1_value_text),
+                    tidesInfo.getHighTide1());
+        } else {
+            highTideText = String.format(context.getString(R.string.high_tide_2_values_text),
+                    tidesInfo.getHighTide1(), tidesInfo.getHighTide2());
+        }
         views.setTextViewText(R.id.textViewHighTide, highTideText);
 
-        String lowTideText = String.format(context.getString(R.string.low_tide_text),
-                tidesInfo.getLowTide1(), tidesInfo.getLowTide2());
+        String lowTideText;
+        if (tidesInfo.getLowTide2() == null) {
+            lowTideText = String.format(context.getString(R.string.low_tide_1_value_text),
+                    tidesInfo.getLowTide1());
+        } else {
+            lowTideText = String.format(context.getString(R.string.low_tide_2_values_text),
+                    tidesInfo.getLowTide1(), tidesInfo.getLowTide2());
+        }
         views.setTextViewText(R.id.textViewLowTide, lowTideText);
 
         String lastUpdatedText = String.format(context.getString(R.string.last_updated_text),
                 tidesInfo.getLastUpdatedTimeFormatted());
         views.setTextViewText(R.id.textViewLastUpdated, lastUpdatedText);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
-    private static TidesInfo getDummyTidesInfo(){
+    /**
+     * Returns the location that the user set when configuring the widget.
+     */
+    private static Location getLocation() {
+        //TODO: Implement this properly
+        return new Location("510P", "Testort");
+    }
+
+    /**
+     * Returns true iff the widget is either updated for the first time or after a manual refresh.
+     */
+    private static boolean isManualOrInitialRefresh(){
+        // TODO: Implement this properly.
+        return true;
+    }
+
+    private static TidesInfo getDummyTidesInfo() {
         return new TidesInfo(
                 "123214",
                 "Bremerhaven / Alter Leuchtturm",
