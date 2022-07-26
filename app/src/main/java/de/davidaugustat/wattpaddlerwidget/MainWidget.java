50cb4c17116a719e947f3a1cec01383bc78ed493
David Augustat <code@davidaugustat.com>
2022-07-26 18:28:44 +0200

    Improved how widget handles network errors.
    - When network error occurs after automatic update: Widget remains
    unchanged, user doesn't get informed.
    - When network error occurs after manual update through the refresh
    button: Toast gets displayed to inform the user about the error.

--- app/src/main/java/de/davidaugustat/wattpaddlerwidget/MainWidget.java
+++ app/src/main/java/de/davidaugustat/wattpaddlerwidget/MainWidget.java
@@ -18,14 +18,21 @@ import androidx.annotation.StringRes;
  */
 public class MainWidget extends AppWidgetProvider {
 
+    /**
+     * Updates the app widget with the provided ID.
+     *
+     * @param appWidgetId ID of the widget to update
+     * @param isManual true iff the update was triggered by user interaction, i.e. the user clicked
+     *                 the refresh button.
+     */
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+                                int appWidgetId, boolean isManual) {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main_widget);
 
         views.setOnClickPendingIntent(R.id.buttonUpdate, getPendingSelfIntent(context, appWidgetId));
         setUpOpenAppOnClick(views, context);
-        refreshWidget(views, context, appWidgetManager, appWidgetId, isManualOrInitialRefresh());
+        refreshWidget(views, context, appWidgetManager, appWidgetId, isManual);
 
         Log.d("Updating widget", "Updating widget with ID " + appWidgetId);
     }
@@ -34,7 +41,7 @@ public class MainWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            updateAppWidget(context, appWidgetManager, appWidgetId, false);
         }
     }
 
@@ -50,7 +57,7 @@ public class MainWidget extends AppWidgetProvider {
 
     /**
      * Gets called when one or more app widgets get deleted.
-     *
+     * <p>
      * Deletes the locations associated with these widget(s) from the shared preferences as they
      * are not needed anymore.
      *
@@ -59,7 +66,7 @@ public class MainWidget extends AppWidgetProvider {
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         super.onDeleted(context, appWidgetIds);
-        for(int appWidgetId : appWidgetIds){
+        for (int appWidgetId : appWidgetIds) {
             SharedPreferencesHelper.deleteLocation(appWidgetId, context);
         }
     }
@@ -67,65 +74,63 @@ public class MainWidget extends AppWidgetProvider {
     /**
      * Loads the data from the API and displays it on the widget.
      *
-     * @param views RemoteViews representing the widget
-     * @param context Context used to get strings
+     * @param views            RemoteViews representing the widget
+     * @param context          Context used to get strings
      * @param appWidgetManager AppWidgetManager used to update the widget
-     * @param appWidgetId ID of the widget to update
-     * @param isManualOrInitialUpdate true iff the widget was manually or initially updated. In this
-     *                                case a info message "Network error" is displayed on the widget
-     *                                to inform the user about the error. If the widget was
-     *                                automatically updated, no error message is displayed but the
-     *                                widget remains unchanged (continues to display old data).
+     * @param appWidgetId      ID of the widget to update
+     * @param isManual         true iff the widget was manually updated. In this case a info toast
+     *                         "Network error" is displayed to inform the user about the error. If
+     *                         the widget was automatically updated, no error message is displayed
+     *                         but the widget remains unchanged (continues to display old data).
      */
     private static void refreshWidget(RemoteViews views, Context context, AppWidgetManager appWidgetManager, int appWidgetId,
-                                      boolean isManualOrInitialUpdate) {
+                                      boolean isManual) {
         try {
             Location location = SharedPreferencesHelper.getLocation(appWidgetId, context);
             new DataFetcher(context).fetchTidesDataSingleDay(location, tidesInfo -> {
                         updateWidgetLayout(views, context, appWidgetManager, appWidgetId, tidesInfo);
                         Log.d("Tides Info", tidesInfo.toString());
+                        if (isManual) {
+                            Toast.makeText(context, context.getString(R.string.widget_updated_toast_text), Toast.LENGTH_SHORT).show();
+                        }
                     },
                     errorMessage -> {
-                        if (isManualOrInitialUpdate) {
-                            //updateWidgetLayoutAtError(R.string.no_network_text,views, context, appWidgetManager, appWidgetId);
-                            updateWidgetLayoutAtError(errorMessage, views, appWidgetManager, appWidgetId);
-                        }
+                        updateWidgetLayoutAtError(errorMessage, views, appWidgetManager, appWidgetId);
                         Log.e("Error fetching tides", errorMessage);
+                        if (isManual) {
+                            Toast.makeText(context, context.getString(R.string.error_updating_toast_text), Toast.LENGTH_SHORT).show();
+                        }
                     });
-        } catch (IllegalArgumentException exception){
+        } catch (IllegalArgumentException exception) {
             Log.d("No location", "No location stored for widget ID" + appWidgetId);
-            updateWidgetLayoutAtError(R.string.not_set_up_text, views, context, appWidgetManager, appWidgetId);
+            updateWidgetLayoutAtError(exception.toString(), views, appWidgetManager, appWidgetId);
         }
     }
 
     /**
-     * Displays an message on the widget. This can be used to inform the user about
-     * an error that ocurred during update.
+     * Updates the widget but does not apply any changes. This is just to make sure that all changes
+     * that were made in the {@link #updateAppWidget(Context, AppWidgetManager, int, boolean)} are
+     * committed.
      *
-     * @param errorString String for the error message
-     * @param views RemoteViews representing the widget
-     * @param appWidgetManager AppWidgerManager used to update the widget.
-     * @param appWidgetId ID of the widget.
+     * <p>This is especially important to ensure that the refresh button gets set up even in case that
+     * a network error occurs at the very first update of the widget. In this case the user should
+     * still be able to perform a refresh.
+     *
+     * <p>Additionally, if {@link Constants#SHOW_DEBUG} is true, the error message gets printed to
+     * the debug text view.
+     *
+     * @param errorString      String for the error message
+     * @param views            RemoteViews representing the widget
+     * @param appWidgetManager AppWidgetManager used to update the widget.
+     * @param appWidgetId      ID of the widget.
      */
     private static void updateWidgetLayoutAtError(String errorString, RemoteViews views,
                                                   AppWidgetManager appWidgetManager, int appWidgetId) {
-        views.setTextViewText(R.id.textViewLocation, errorString);
+        if (Constants.SHOW_DEBUG) {
+            views.setViewVisibility(R.id.textViewDebug, View.VISIBLE);
+            views.setTextViewText(R.id.textViewDebug, errorString);
+        }
         appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
-
-    /**
-     * Displays an message on the widget. This can be used to inform the user about
-     * an error that ocurred during update.
-     *
-     * @param errorStringId String for the error message
-     * @param views RemoteViews representing the widget
-     * @param context Context used to get strings
-     * @param appWidgetManager AppWidgerManager used to update the widget.
-     * @param appWidgetId ID of the widget.
-     */
-    private static void updateWidgetLayoutAtError(@StringRes int errorStringId, RemoteViews views, Context context,
-                                                  AppWidgetManager appWidgetManager, int appWidgetId) {
-        updateWidgetLayoutAtError(context.getString(errorStringId), views, appWidgetManager, appWidgetId);
     }
 
     /**
@@ -136,17 +141,21 @@ public class MainWidget extends AppWidgetProvider {
     private static void updateWidgetLayout(RemoteViews views, Context context,
                                            AppWidgetManager appWidgetManager, int appWidgetId,
                                            TidesInfo tidesInfo) {
+
+        views.setViewVisibility(R.id.widgetInfoLayout, View.VISIBLE);
+        views.setViewVisibility(R.id.textViewStatus, View.GONE);
+
         views.setTextViewText(R.id.textViewLocation, tidesInfo.getLocationName());
         views.setTextViewText(R.id.textViewDate, tidesInfo.getDateFormatted());
         views.setTextViewText(R.id.textViewHighTide, getHighTidesText(context, tidesInfo));
         views.setTextViewText(R.id.textViewLowTide, getLowTidesText(context, tidesInfo));
 
         // Show last updated text only for debug purposes:
-        if(Constants.SHOW_LAST_UPDATED) {
+        if (Constants.SHOW_DEBUG) {
             String lastUpdatedText = String.format(context.getString(R.string.last_updated_text),
                     tidesInfo.getLastUpdatedTimeFormatted());
-            views.setTextViewText(R.id.textViewLastUpdated, lastUpdatedText);
-            views.setViewVisibility(R.id.textViewLastUpdated, View.VISIBLE);
+            views.setTextViewText(R.id.textViewDebug, lastUpdatedText);
+            views.setViewVisibility(R.id.textViewDebug, View.VISIBLE);
         }
 
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -158,7 +167,7 @@ public class MainWidget extends AppWidgetProvider {
     @NonNull
     private static String getLowTidesText(Context context, TidesInfo tidesInfo) {
         String lowTideText = "";
-        switch (tidesInfo.getNumberOfLowTides()){
+        switch (tidesInfo.getNumberOfLowTides()) {
             case 2:
                 lowTideText = String.format(context.getString(R.string.low_tides_text),
                         tidesInfo.getLowTide1Formatted(), tidesInfo.getLowTide2Formatted());
@@ -180,10 +189,10 @@ public class MainWidget extends AppWidgetProvider {
     @NonNull
     private static String getHighTidesText(Context context, TidesInfo tidesInfo) {
         String highTideText = "";
-        switch (tidesInfo.getNumberOfHighTides()){
+        switch (tidesInfo.getNumberOfHighTides()) {
             case 2:
                 highTideText = String.format(context.getString(R.string.high_tides_text),
-                    tidesInfo.getHighTide1Formatted(), tidesInfo.getHighTide2Formatted());
+                        tidesInfo.getHighTide1Formatted(), tidesInfo.getHighTide2Formatted());
                 break;
             case 1:
                 highTideText = String.format(context.getString(R.string.high_tides_text),
@@ -198,7 +207,7 @@ public class MainWidget extends AppWidgetProvider {
 
     /**
      * Called when the underlying broadcast receiver of this app widget provider receives an intent.
-     *
+     * <p>
      * This method is used to receive intents that are sent when the refresh button of the widget
      * is clicked. In this case the widget gets updated.
      */
@@ -206,15 +215,15 @@ public class MainWidget extends AppWidgetProvider {
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
 
-        if(intent.getAction().equals(Constants.WIDGET_REFRESH_BUTTON_ACTION)){
+        if (intent.getAction().equals(Constants.WIDGET_REFRESH_BUTTON_ACTION)) {
             int appWidgetId = intent.getIntExtra(Constants.APP_WIDGET_ID_EXTRA, Constants.INVALID_APP_WIDGET_ID);
-            if(appWidgetId == Constants.INVALID_APP_WIDGET_ID){
+            if (appWidgetId == Constants.INVALID_APP_WIDGET_ID) {
                 Log.e("OnReceive", "App widget ID was not passed with intent.");
                 return;
             }
             Log.d("Widget onReceive", "Refresh button clicked for widget with ID " + appWidgetId);
             AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            updateAppWidget(context, appWidgetManager, appWidgetId, true);
         }
     }
 
@@ -222,9 +231,10 @@ public class MainWidget extends AppWidgetProvider {
      * Returns a PendingIntent that points to this AppWidgetReceiver. When the PendingIntent
      * is executed, the onReceive() method of this class gets called. This is used to do something
      * when the refresh button of the widget gets pressed.
-     *
+     * <p>
      * The pending intent includes the Constants.WIDGET_REFRESH_BUTTON_ACTION as well as the app
      * widget ID of the widget on which the button was pressed.
+     *
      * @param appWidgetId ID of the app widget that should be updated when the refresh button is
      *                    pressed.
      */
@@ -240,8 +250,8 @@ public class MainWidget extends AppWidgetProvider {
      * If the Wattpaddler app (com.embarcadero.Wattpaddler) is installed on the device,
      * the main layout of the widget gets a click listener that opens this app when clicked.
      */
-    private static void setUpOpenAppOnClick(RemoteViews views, Context context){
-        if(AppPackageDetectionHelper.isWattpaddlerAppInstalled(context)){
+    private static void setUpOpenAppOnClick(RemoteViews views, Context context) {
+        if (AppPackageDetectionHelper.isWattpaddlerAppInstalled(context)) {
             Intent intent = context.getPackageManager()
                     .getLaunchIntentForPackage(Constants.WATTPADDLER_APP_PACKAGE_NAME);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,
@@ -251,13 +261,5 @@ public class MainWidget extends AppWidgetProvider {
             Log.d("App not installed", "Wattpaddler app not is installed on the device. " +
                     "Not setting up a click listener for it.");
         }
-    }
-
-    /**
-     * Returns true iff the widget is either updated for the first time or after a manual refresh.
-     */
-    private static boolean isManualOrInitialRefresh(){
-        // TODO: Implement this properly.
-        return true;
     }
 }
