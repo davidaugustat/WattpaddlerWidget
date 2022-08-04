d843bcd72849501a65de4abff4389e7fcbc641c3
David Augustat <code@davidaugustat.com>
2022-08-04 02:21:12 +0200

    Created TideTimeFactory.java

---
+++ app/src/main/java/de/davidaugustat/wattpaddlerwidget/TideTimeFactory.java
@@ -0,0 +1,16 @@
+package de.davidaugustat.wattpaddlerwidget;
+
+/**
+ * Constructs a TideTime object whose subtype is dependent on the properties of the input data.
+ */
+public class TideTimeFactory {
+    public static TideTime getTideTime(String dateString, String timeString, String targetDateString){
+        if (dateString == null || timeString == null) {
+            return new NonExistentTideTime();
+        }
+        if(dateString.equals(targetDateString)){
+            return new NormalTideTime(dateString, timeString);
+        }
+        return new ShiftedTideTime();
+    }
+}
